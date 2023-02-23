e3380982f1aad435079305efe182e170d2d2eab8
Jaime Niswonger <jniswonger@keyholesoftware.com>
2023-02-22 20:48:56 -0600

    updated react template

--- templates/react-app/content/src/App.tsx
+++ templates/react-app/content/src/App.tsx
@@ -1,23 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <picture>
+          <img src="/logo.webp" alt="logo" />
+        </picture>
+        <h3>${{ values.component_id }}</h3>
+        <p>
+          This is the default Keyhole React template.
+        </p>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
